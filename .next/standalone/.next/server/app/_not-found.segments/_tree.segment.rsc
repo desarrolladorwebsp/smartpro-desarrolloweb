51b30d65094bdef2ba34a6521f17b07a704d6f0b
Desarrollador SmartPro <desarrolladorwp@gmail.com>
2026-09-16 23:01:46 -0300

    Deploy standalone Next.js build from local CI

---
+++ .next/standalone/.next/server/app/_not-found.segments/_tree.segment.rsc
@@ -0,0 +1,2 @@
+:HL["/_next/static/css/b6a110f70c1140e5.css","style"]
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"_not-found","param":null,"prefetchHints":4192,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4256,"slots":null}}}}},"staleTime":300,"buildId":"T2C2SX1uWid53Z40XKqjY"}
--- server.js
+++ server.js
@@ -1,13 +1,2 @@
-const { createServer } = require("node:http");
-const next = require("next");
-
-const port = Number.parseInt(process.env.PORT || "3000", 10);
-const hostname = process.env.HOSTNAME || "0.0.0.0";
-const app = next({ dev: false, hostname, port });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  createServer((request, response) => handle(request, response)).listen(port, hostname, () => {
-    console.log(`SmartPro Desarrollo Web listening on ${hostname}:${port}`);
-  });
-});
+process.chdir(__dirname);
+require("./.next/standalone/server.js");
